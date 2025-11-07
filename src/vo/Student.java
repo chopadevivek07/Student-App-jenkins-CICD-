e810cec0c3118072d8dec8cbe8980612aa5d756e
chopadevivek07 <chopadevivek4466@gmail.com>
2025-11-07 22:01:35 +0530

    updated

--- src/vo/Student.java
+++ src/vo/Student.java
@@ -1,72 +1,33 @@
-package vo;
+package com.srk.vo;
 
 public class Student {
-	private int studentId;
-	private String studentName;
-	private String studentAddr;
-	private String age;
-	private String qualification;
-	private String percentage;
-	private String yearPassed;
-	
-	public Student(){}
+    private int id;
+    private String name;
+    private String address;
+    private int age;
+    private String qualification;
+    private double percentage;
+    private int yearPassed;
 
-	public int getStudentId() {
-		return studentId;
-	}
+    // Getters and setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
-	public void setStudentId(int studentId) {
-		this.studentId = studentId;
-	}
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-	public String getStudentName() {
-		return studentName;
-	}
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
 
-	public void setStudentName(String studentName) {
-		this.studentName = studentName;
-	}
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
 
-	public String getStudentAddr() {
-		return studentAddr;
-	}
+    public String getQualification() { return qualification; }
+    public void setQualification(String qualification) { this.qualification = qualification; }
 
-	public void setStudentAddr(String studentAddr) {
-		this.studentAddr = studentAddr;
-	}
+    public double getPercentage() { return percentage; }
+    public void setPercentage(double percentage) { this.percentage = percentage; }
 
-	public String getAge() {
-		return age;
-	}
-
-	public void setAge(String age) {
-		this.age = age;
-	}
-
-	public String getQualification() {
-		return qualification;
-	}
-
-	public void setQualification(String qualification) {
-		this.qualification = qualification;
-	}
-
-	public String getPercentage() {
-		return percentage;
-	}
-
-	public void setPercentage(String percentage) {
-		this.percentage = percentage;
-	}
-
-	public String getYearPassed() {
-		return yearPassed;
-	}
-
-	public void setYearPassed(String yearPassed) {
-		this.yearPassed = yearPassed;
-	}
-
-	
+    public int getYearPassed() { return yearPassed; }
+    public void setYearPassed(int yearPassed) { this.yearPassed = yearPassed; }
 }
- 
